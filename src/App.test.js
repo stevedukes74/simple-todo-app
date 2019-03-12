4c1c0140b422bcd7691c9cf72ed6974636b3c2b8
Steve Dukes <stdukes@gmail.com>
2019-03-12 19:11:09 -0400

    App crash test failing

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import App from './App';
+import { createStore, applyMiddleware } from 'redux';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
